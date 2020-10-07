91cfa0c9e1c59c1a48f20a3a82a44db62354db36
mrz1836 <mrz1818@gmail.com>
2020-10-06 20:14:53 -0400

    Bump version

--- client_test.go
+++ client_test.go
@@ -110,7 +110,7 @@ func ExampleNewClient() {
 		return
 	}
 	fmt.Printf("loaded client: %s", client.Options.UserAgent)
-	// Output:loaded client: go-paymail: v0.0.8
+	// Output:loaded client: go-paymail: v0.0.9
 }
 
 // BenchmarkNewClient benchmarks the method NewClient()
@@ -216,7 +216,7 @@ func ExampleDefaultClientOptions() {
 		return
 	}
 	fmt.Printf("loaded options: %s", options.UserAgent)
-	// Output:loaded options: go-paymail: v0.0.8
+	// Output:loaded options: go-paymail: v0.0.9
 }
 
 // BenchmarkDefaultClientOptions benchmarks the method DefaultClientOptions()
