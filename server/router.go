b274190ba92a3d04ac81e30a6d628b0a508d9e29
mrz1836 <mrz1818@gmail.com>
2021-07-07 11:07:29 -0400

    Minor spelling fixes, removed deprecated linters

--- server/router.go
+++ server/router.go
@@ -8,13 +8,13 @@ import (
 	"github.com/tonicpow/go-paymail"
 )
 
-// Handlers is used to isolate loading the routes (used for testing)
+// Handlers are used to isolate loading the routes (used for testing)
 func Handlers() *httprouter.Router {
 
 	// Create a new router
 	r := apirouter.New()
 
-	// Turned off all CORs - should be accessed outside of a browser
+	// Turned off all CORs - should be accessed outside a browser
 	r.CrossOriginEnabled = false
 	r.CrossOriginAllowCredentials = false
 	r.CrossOriginAllowOriginAll = false
@@ -51,43 +51,43 @@ func registerBasicRoutes(router *apirouter.Router) {
 // registerPaymailRoutes will register all paymail related routes
 func registerPaymailRoutes(router *apirouter.Router) {
 
-	// Set the capabilities (service discovery)
+	// Capabilities (service discovery)
 	router.HTTPRouter.GET(
 		"/.well-known/"+paymail.DefaultServiceName,
 		router.Request(showCapabilities),
 	)
 
-	// Set the PKI request (public key information)
+	// PKI request (public key information)
 	router.HTTPRouter.GET(
 		"/"+paymailAPIVersion+"/"+paymail.DefaultServiceName+"/id/:paymailAddress",
 		router.Request(showPKI),
 	)
 
-	// Set the Verify PubKey request (public key verification to paymail address)
+	// Verify PubKey request (public key verification to paymail address)
 	router.HTTPRouter.GET(
 		"/"+paymailAPIVersion+"/"+paymail.DefaultServiceName+"/verify-pubkey/:paymailAddress/:pubKey",
 		router.Request(verifyPubKey),
 	)
 
-	// Set the Payment Destination request (address resolution)
+	// Payment Destination request (address resolution)
 	router.HTTPRouter.POST(
 		"/"+paymailAPIVersion+"/"+paymail.DefaultServiceName+"/address/:paymailAddress",
 		router.Request(resolveAddress),
 	)
 
-	// Set the Public Profile request (returns Name & Avatar)
+	// Public Profile request (returns Name & Avatar)
 	router.HTTPRouter.GET(
 		"/"+paymailAPIVersion+"/"+paymail.DefaultServiceName+"/public-profile/:paymailAddress",
 		router.Request(publicProfile),
 	)
 
-	// Set the P2P Destination request (returns output & reference)
+	// P2P Destination request (returns output & reference)
 	router.HTTPRouter.POST(
 		"/"+paymailAPIVersion+"/"+paymail.DefaultServiceName+"/p2p-payment-destination/:paymailAddress",
 		router.Request(p2pDestination),
 	)
 
-	// Set the P2P Receive Tx request (receives the P2P transaction, broadcasts, returns tx_id)
+	// P2P Receive Tx request (receives the P2P transaction, broadcasts, returns tx_id)
 	router.HTTPRouter.POST(
 		"/"+paymailAPIVersion+"/"+paymail.DefaultServiceName+"/receive-transaction/:paymailAddress",
 		router.Request(p2pReceiveTx),
