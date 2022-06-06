45e41d734517406ab6dcf725d250c99e6c1ddcf1
mrz1836 <mrz1818@gmail.com>
2022-06-06 15:26:53 -0400

    Bumped version

--- client_test.go
+++ client_test.go
@@ -205,7 +205,7 @@ func ExampleNewClient() {
 		return
 	}
 	fmt.Printf("loaded client: %s", client.GetOptions().userAgent)
-	// Output:loaded client: go-paymail: v0.7.7
+	// Output:loaded client: go-paymail: v0.7.8
 }
 
 // BenchmarkNewClient benchmarks the method NewClient()
